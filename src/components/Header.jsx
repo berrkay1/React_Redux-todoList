83cc86aa1e29f4787e25b4ee5c96625fd301ad82
BERKAY GÜL <berrkaygul@gmail.com>
2022-05-11 21:00:09 +0300

    firs commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="header">
+		<h1>todos</h1>
+		<form>
+			<input className="new-todo" placeholder="What needs to be done?" autofocus/>
+		</form>
+	</header>
+  )
+}
+
+export default Header
